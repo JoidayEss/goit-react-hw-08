4c100817fbed44dd79015d75c502d9eca2116189
Артем Томащук <tomashhukartem@gmail.com>
2024-12-09 17:29:39 +0200

    new chande code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,15 @@
+import s from "./HomePage.module.css";
+import catImage from "../../assets/cool-klev-club-25yo-p-prikolnie-kartinki-kot-i-telefon-4.jpg";
+
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to Contacts Editor</h1>
+      <h2 className={s.paragraph}>
+        Hello everyone, this is a site where you can register and add or remove
+        your contacts
+      </h2>
+      <img src={catImage} alt="cat" />
     </div>
   );
 };
